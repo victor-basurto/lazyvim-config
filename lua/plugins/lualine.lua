a8b5829321af66a36236f92f752664cb834b293a
Victor Basurto <my.mvplace@gmail.com>
2025-04-17 00:16:51 -0700

    added new packge for telescope and mason

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,5 +3,12 @@ return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     requires = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "dracula",
+        },
+      })
+    end,
   },
 }
