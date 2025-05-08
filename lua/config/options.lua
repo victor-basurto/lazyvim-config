75197453431d0262f2a59d6e3be78cc9727a64ab
Victor Basurto <my.mvplace@gmail.com>
2025-05-08 12:53:54 -0700

    configured conceallevel for json, configured undercurl and disabled mouse

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,9 @@
 -- Add any additional options here
 vim.g.lazyvim_picker = "telescope"
 -- vim.g.lazyvim_prettier_needs_config = true
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+-- no mouse
+vim.opt.mouse = ""
