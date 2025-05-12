ac4f42668065159a4b46cceb3a86859a5fd01134
Victor Basurto <my.mvplace@gmail.com>
2025-05-12 10:57:57 -0700

    typo in mason shellcheck

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,7 +6,7 @@ return {
       ensure_installed = {
         "gitui",
         "stylua",
-        "spellcheck",
+        "shellcheck",
         "tailwindcss-language-server",
         "shfmt",
         "luacheck",
