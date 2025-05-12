a4f16610cb8b9c9d9ee74ecfd5706e678bb84291
Victor Basurto <my.mvplace@gmail.com>
2025-05-12 14:16:05 -0700

    Markdown linting and preview enabled and lsp installed for md

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -10,6 +10,8 @@ return {
         "tailwindcss-language-server",
         "shfmt",
         "luacheck",
+        "markdownlint-cli2",
+        "markdown-toc",
       },
     },
     config = function()
@@ -84,6 +86,7 @@ return {
       lspconfig.html.setup({})
       lspconfig.jsonls.setup({})
       lspconfig.yamlls.setup({})
+      lspconfig.marksman.setup({})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
